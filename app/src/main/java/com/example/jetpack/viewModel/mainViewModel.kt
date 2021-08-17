1dd7235cd244d4ff602fb70f7883ef401d4e23ac
chaips <ar0439708@gmail.com>
2021-08-17 18:37:48 -0500

    Repository.kt created

--- app/src/main/java/com/example/jetpack/viewModel/mainViewModel.kt
+++ app/src/main/java/com/example/jetpack/viewModel/mainViewModel.kt
@@ -2,7 +2,8 @@ package com.example.jetpack.viewModel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class mainViewModel:ViewModel() {
+class mainViewModel @Inject constructor():ViewModel() {
 }
